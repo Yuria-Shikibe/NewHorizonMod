3593e64ca98c65f3a360f6671b3a229814a66a17
Yuria-Shikibe <3415339403@qq.com>
2021-07-12 14:45:54 +0800

    1.8.4

--- src/newhorizon/block/defence/AssignOverdrive.java
+++ src/newhorizon/block/defence/AssignOverdrive.java
@@ -34,12 +34,12 @@ public class AssignOverdrive extends OverdriveProjector{
 		sync = true;
 		config(Integer.class, (Cons2<AssignOverdriveBuild, Integer>)AssignOverdriveBuild::linkPos);
 		config(Point2.class, (Cons2<AssignOverdriveBuild, Point2>)AssignOverdriveBuild::linkPos);
-		config(Point2[].class, (entity, point2s) -> {
-			for(Point2 p : point2s){
-				entity.configure(Point2.pack(p.x + entity.tileX(), p.y + entity.tileY()));
-			}
-		});
-		
+//		config(Point2[].class, (entity, point2s) -> {
+//			for(Point2 p : point2s){
+//				entity.configure(Point2.pack(p.x + entity.tileX(), p.y + entity.tileY()));
+//			}
+//		});
+//
 	}
 	
 	@Override
@@ -62,15 +62,14 @@ public class AssignOverdrive extends OverdriveProjector{
 		protected float smoothEfficiency;
 		protected IntSeq targets = new IntSeq(maxLink);
 		
-		@Override
-		public Point2[] config(){
-			if(Vars.net.active())return new Point2[]{};
-			Point2[] out = new Point2[targets.size];
-			for(int i = 0; i < out.length; i++){
-				out[i] = Point2.unpack(targets.get(i)).sub(tile.x, tile.y);
-			}
-			return out;
-		}
+//		@Override
+//		public Point2[] config(){
+//			Point2[] out = new Point2[targets.size];
+//			for(int i = 0; i < out.length; i++){
+//				out[i] = Point2.unpack(targets.get(i)).sub(tile.x, tile.y);
+//			}
+//			return out;
+//		}
 		
 		@Override
 		public void draw(){
