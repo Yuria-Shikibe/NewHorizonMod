7369cbbded6d32cad4a2c3acba8400b2227b55c5
Lao_Hua_Ji <laohuaji233@gmail.com>
2024-10-18 10:58:49 +0800

    bridge update

--- src/newhorizon/expand/block/distribution/AdaptItemBridge.java
+++ src/newhorizon/expand/block/distribution/AdaptItemBridge.java
@@ -2,13 +2,17 @@ package newhorizon.expand.block.distribution;
 
 import arc.Core;
 import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
 import arc.graphics.g2d.Lines;
 import arc.graphics.g2d.TextureRegion;
+import arc.math.Angles;
 import arc.math.Mathf;
 import arc.math.geom.Geometry;
 import arc.math.geom.Point2;
+import arc.math.geom.Vec2;
 import arc.struct.Queue;
 import arc.struct.Seq;
+import arc.util.Log;
 import arc.util.Time;
 import arc.util.Tmp;
 import mindustry.entities.TargetPriority;
@@ -31,9 +35,10 @@ import static mindustry.Vars.tilesize;
 public class AdaptItemBridge extends AdaptBlock {
     public float itemPerSecond = 3f;
     public int maxLength;
-    public TextureRegion[] edgeRegion;
-    public TextureRegion headRegion;
+    public TextureRegion[] edgeRegion, headRegion ,arrowRegions;
     public AdaptConveyor conveyor;
+
+    private final Vec2 tmp0 = new Vec2(), tmp1 = new Vec2(), tmp2 = new Vec2(), tmp3 = new Vec2();
 
     public AdaptItemBridge(String name) {
         super(name);
@@ -67,7 +72,8 @@ public class AdaptItemBridge extends AdaptBlock {
     public void load() {
         super.load();
         edgeRegion = SpriteUtil.splitRegionArray(Core.atlas.find(name + "-edge"), 32, 32, 1);
-        headRegion = Core.atlas.find(name + "-head");
+        arrowRegions = SpriteUtil.splitRegionArray(Core.atlas.find(name + "-arrow"), 32, 32, 1, false);
+        headRegion = SpriteUtil.splitRegionArray(Core.atlas.find(name + "-head0"), 8, 36);
     }
 
     public float framePeriod(){
@@ -81,8 +87,16 @@ public class AdaptItemBridge extends AdaptBlock {
         public static final int RECEIVE_STATUS = 0;
         public static final int SEND_STATUS = 1;
 
+        //used to control the bridge
+        public float bridgeAng;
         public float bridgeDst;
         public int bridgeSeg;
+
+        //used to control the rotation center of the corner
+        public Vec2 bridgeRotCenter = new Vec2();
+        public float bridgeRotDst;
+        public float bridgeRotAng;
+        public float bridgeRotClip;
 
         public Queue<ItemStacker> bridgeItems;
 
@@ -112,12 +126,21 @@ public class AdaptItemBridge extends AdaptBlock {
                 setReceiver(build);
                 build.setSender(this);
 
-                Tmp.v1.trns(rotdeg(), tilesize/2f).add(this);
-                Tmp.v2.trns(other.rotdeg(), -tilesize/2f).add(other);
-
-                bridgeDst = MathUtil.dst(Tmp.v1, Tmp.v2);
+                bridgeAng = MathUtil.angle(this, other);
+                bridgeDst = MathUtil.dst(this, other) - tilesize;
                 bridgeSeg = Mathf.round(bridgeDst / tilesize);
 
+                bridgeRotAng = (rotdeg() + 180 + bridgeAng)/2f;
+                bridgeRotClip = Angles.angleDist(bridgeAng, bridgeRotAng);
+                if (Angles.within(bridgeRotClip, 90, 1f)){
+                    bridgeRotCenter.set(this);
+                    bridgeRotAng = rotdeg();
+                    bridgeRotDst = 4f;
+                }else {
+                    float dst = tilesize/2f / Mathf.cosDeg(bridgeRotClip);
+                    bridgeRotCenter.trns(bridgeRotAng, dst).add(this);
+                    bridgeRotDst = dst;
+                }
                 return true;
             }
             return true;
@@ -225,6 +248,7 @@ public class AdaptItemBridge extends AdaptBlock {
             progress = 0f;
             items.add(item, count);
         }
+
         @Override
         public boolean acceptItem(Building source, Item item) {
             //check if connected and input&output pos
@@ -309,25 +333,61 @@ public class AdaptItemBridge extends AdaptBlock {
             if (conveyor == null) return;
             int index = (int)((((Time.time) % conveyor.framePeriod()) / conveyor.framePeriod()) * 16);
             Draw.z(Layer.blockUnder - 0.1f);
-            Draw.rect(conveyor.baseRegions[0], x, y, rotdeg());
-            Draw.rect(conveyor.arrowRegions[index + 16], x, y, rotdeg());
-            Draw.rect(conveyor.arrowRegions[index], x, y, rotdeg());
+            if (other != null){
+                drawCorner();
+                drawBridge();
+            }
+            /*
+            if (other != null){
+                drawBridge();
+                if (status == SEND_STATUS){
+                    //Draw.rect(headRegion[0], x, y, rotdeg());
+                    Draw.rect(arrowRegions[index], x, y, rotdeg());
+                }
+                if (status == RECEIVE_STATUS){
+                    //Draw.rect(headRegion[1], x, y, rotdeg());
+                    Draw.rect(arrowRegions[index + 16], x, y, rotdeg());
+                }
+            }else {
+                //Draw.rect(headRegion[0], x, y, rotdeg());
+                Draw.rect(arrowRegions[index], x, y, rotdeg());
+            }
+
+             */
             float z = Draw.z();
-            Draw.z(z + 0.0001f);
-            Draw.rect(conveyor.edgeRegions[0], x, y, rotdeg());
+            //Draw.z(z + 0.0001f);
+            //Draw.rect(conveyor.edgeRegions[0], x, y, rotdeg());
 
             if(stackItem() != null){
-                Draw.z(Layer.block + 0.08f);
+                Draw.z(Layer.block + 0.11f);
                 Tmp.v1.set(Geometry.d4x(rotation + 2) * tilesize / 2f, Geometry.d4y(rotation + 2) * tilesize / 2f)
                     .lerp(Geometry.d4x(rotation) * tilesize / 2f, Geometry.d4y(rotation) * tilesize / 2f, progress / framePeriod());
                 Draw.rect(stackItem().fullIcon, x + Tmp.v1.x, y + Tmp.v1.y, itemSize, itemSize);
                 DrawUtil.drawText(stackCount() + "", x + Tmp.v1.x, y + Tmp.v1.y, 1f);
             }
+            Draw.z(Layer.block + 0.2f);
+            //Lines.stroke(1f);
+            //Lines.lineAngle(x, y, rotdeg() + 180, 20);
+            //Lines.lineAngle(x, y, bridgeAng, 20);
+            //Lines.lineAngle(x, y, bridgeRotAng, bridgeRotDst);
+        }
 
-            if (other != null){
-                drawBridge();
-            }else {
-                Draw.rect(headRegion, x, y, 90);
+        public void drawCorner(){
+            float len = bridgeRotDst * Mathf.sinDeg(bridgeRotClip);
+            float ang = (90 - bridgeRotClip)/2f;
+            float start = bridgeRotAng + 180 - ang * 2;
+            for (int i = 0; i < 4; i++){
+                tmp0.trns(start + i * ang, len - 4.5f).add(bridgeRotCenter);
+                tmp1.trns(start + i * ang, len + 4.5f).add(bridgeRotCenter);
+                tmp2.trns(start + (i + 1) * ang, len + 4.5f).add(bridgeRotCenter);
+                tmp3.trns(start + (i + 1) * ang, len - 4.5f).add(bridgeRotCenter);
+                Fill.quad(
+                    headRegion[i],
+                    tmp0.x, tmp0.y,
+                    tmp1.x, tmp1.y,
+                    tmp2.x, tmp2.y,
+                    tmp3.x, tmp3.y
+                );
             }
         }
 
@@ -343,35 +403,27 @@ public class AdaptItemBridge extends AdaptBlock {
             int index = (int)((((Time.time) % conveyor.framePeriod()) / conveyor.framePeriod()) * 16);
             float dstLen = bridgeDst / bridgeSeg;
 
-            Tmp.v1.trns(rotdeg(), tilesize/2f).add(this);
-            Tmp.v2.trns(other.rotdeg(), -tilesize/2f).add(other);
-            float angle = MathUtil.angle(Tmp.v1, Tmp.v2);
             for (int i = 0; i < bridgeSeg; i++){
-                float segDst = dstLen * i + dstLen/2f;
+                float segDst = dstLen * i + dstLen;
                 int idx = i % 2;
-                Tmp.v1.trns(angle, segDst).add(this);
-                Tmp.v2.trns(rotdeg(), tilesize/2f);
-                Tmp.v1.add(Tmp.v2);
+                Tmp.v1.trns(bridgeAng, segDst).add(this);
                 Draw.z(Layer.block + 0.1f);
-                Draw.rect(edgeRegion[idx], Tmp.v1.x, Tmp.v1.y, dstLen, tilesize, angle);
-                Draw.rect(conveyor.arrowRegions[index], Tmp.v1.x, Tmp.v1.y, dstLen, tilesize, angle);
-                Draw.rect(conveyor.arrowRegions[index + 16], Tmp.v1.x, Tmp.v1.y, dstLen, tilesize, angle);
+                Draw.rect(edgeRegion[idx], Tmp.v1.x, Tmp.v1.y, dstLen, tilesize, bridgeAng);
+                Draw.rect(arrowRegions[index + 32], Tmp.v1.x, Tmp.v1.y, dstLen, tilesize, bridgeAng);
             }
             for (int i = 0; i < bridgeItems.size; i++){
                 ItemStacker stack = bridgeItems.get(i);
                 float segDst = ((stack.progress / framePeriod()) / bridgeSeg) * bridgeDst;
-                Tmp.v1.trns(angle, segDst).add(this);
-                Tmp.v2.trns(rotdeg(), tilesize/2f);
-                Tmp.v1.add(Tmp.v2);
+                Tmp.v1.trns(bridgeAng, segDst).add(this);
                 Draw.z(Layer.block + 0.11f);
                 Draw.rect(stack.itemStack.item.fullIcon, Tmp.v1.x, Tmp.v1.y, itemSize, itemSize);
                 DrawUtil.drawText(stack.itemStack.amount + "", Tmp.v1.x, Tmp.v1.y, 1f);
             }
-            Draw.z(Layer.block + 0.09f);
-            Draw.color(Pal.shadow);
-            Tmp.v1.trns(angle, 4f).add(this);
-            Lines.stroke(tilesize);
-            Lines.lineAngle(Tmp.v1.x - tilesize/4f, Tmp.v1.y - tilesize/4f, angle, bridgeDst - tilesize);
+            //Draw.z(Layer.block + 0.09f);
+            //Draw.color(Pal.shadow);
+            //Tmp.v1.trns(bridgeAng, 4f).add(this);
+            //Lines.stroke(tilesize);
+            //Lines.lineAngle(Tmp.v1.x - tilesize/4f, Tmp.v1.y - tilesize/4f, bridgeAng, bridgeDst - tilesize);
             Draw.color();
         }
     }
