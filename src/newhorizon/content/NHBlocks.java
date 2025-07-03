de302318e5fdf1fa68f26d070224a51c4ebd1996
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-07-03 12:06:49 +0800

    Update NHBlocks.java

--- src/newhorizon/content/NHBlocks.java
+++ src/newhorizon/content/NHBlocks.java
@@ -2314,8 +2314,8 @@ public class NHBlocks {
             );
 
             consumePower(100.0F);
-            consumeItems(ItemStack.with(NHItems.thermoCoreNegative, 6, Items.phaseFabric, 4)).optional(true, true);
-            consumeItems(new ItemStack(NHItems.metalOxhydrigen, 4), new ItemStack(NHItems.thermoCorePositive, 6));
+            consumeItems(ItemStack.with(NHItems.thermoCoreNegative, 6, Items.phaseFabric, 6)).optional(true, true);
+            consumeItems(new ItemStack(NHItems.thermoCorePositive, 6), new ItemStack(NHItems.metalOxhydrigen, 6));
             consumeLiquid(NHLiquids.zetaFluidNegative, 8/60f);
             consumeLiquids(new LiquidStack(NHLiquids.zetaFluidPositive, 8/60f)).optional(true, true);
             requirements(Category.power, BuildVisibility.shown, with(NHItems.upgradeSort, 800, NHItems.setonAlloy, 600, NHItems.irayrondPanel, 400, NHItems.presstanium, 1500, Items.surgeAlloy, 250));
