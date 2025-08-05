3e2ee387c77d92b23b32e41fe24db2c4eef15832
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-08-05 12:05:31 +0800

    fix?

--- src/newhorizon/content/NHBlocks.java
+++ src/newhorizon/content/NHBlocks.java
@@ -2287,10 +2287,8 @@ public class NHBlocks {
             itemDuration = 180f;
             ambientSound = Sounds.pulse;
             ambientSoundVolume = 0.1F;
-            toApplyStatus.addAll(
-                    NHStatusEffects.stableOverdrive
-            );
-
+            toApplyStatus.add(NHStatusEffects.stableOverdrive);
+            
             consumePower(100.0F);
             consumeItems(ItemStack.with(NHItems.thermoCoreNegative, 6, Items.phaseFabric, 6)).optional(true, true);
             consumeItems(new ItemStack(NHItems.thermoCorePositive, 6), new ItemStack(NHItems.metalOxhydrigen, 6));
