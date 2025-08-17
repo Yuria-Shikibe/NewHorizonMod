d4f558b7df28685da724f9723271fa41e9f24b62
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-08-17 08:35:57 +0800

    somechange

--- src/newhorizon/content/blocks/DefenseBlock.java
+++ src/newhorizon/content/blocks/DefenseBlock.java
@@ -79,6 +79,7 @@ public class DefenseBlock {
             consumePower(2f);
             consumeLiquid(NHLiquids.xenFluid, 12f / 60f);
             consumeItem(NHItems.phaseFabric).boost();
+            phaseRadiusBoost = 60f;
         }};
 
         heavyRegenProjector = new AdaptRegenProjector("heavy-regen-projector") {{
@@ -96,6 +97,7 @@ public class DefenseBlock {
             consumePower(5f);
             consumeLiquid(NHLiquids.irdryonFluid, 6f / 60f);
             consumeItem(NHItems.ancimembrane).boost();
+            phaseRadiusBoost = 60f;
         }};
 
 
