4a21d7e470d70d38090af788a9fa6df7c33b923c
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-09-15 18:22:31 +0800

    somechange

--- src/newhorizon/content/NHUnitTypes.java
+++ src/newhorizon/content/NHUnitTypes.java
@@ -3907,6 +3907,7 @@ public class NHUnitTypes {
                     width = 24f;
                     drawWidth = 12f;
                     whenShooting = false;
+                    chanceDeflect = 1f;
                 }});
 
                 immunities = ObjectSet.with(NHStatusEffects.weak, NHStatusEffects.emp2, NHStatusEffects.emp3, NHStatusEffects.emp1, NHStatusEffects.scrambler, NHStatusEffects.scannerDown, NHStatusEffects.ultFireBurn, StatusEffects.melting, StatusEffects.burning, StatusEffects.shocked, StatusEffects.electrified);
