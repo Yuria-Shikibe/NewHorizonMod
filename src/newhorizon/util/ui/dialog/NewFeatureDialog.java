1e6331365f5e023b2985a84b305ba4cf77a9fd1b
LaoHuaJi <laohuaji233@gmail.com>
2025-10-01 22:42:56 +0800

    2.0 pre-beta 4, Adapt to 152+

--- src/newhorizon/util/ui/dialog/NewFeatureDialog.java
+++ src/newhorizon/util/ui/dialog/NewFeatureDialog.java
@@ -39,74 +39,8 @@ public class NewFeatureDialog extends BaseDialog {
 
     public static FeatureLog[] getUpdateContent() {
         Seq<FeatureLog> updates = Seq.with(
-                new FeatureLog(0, FeatureLog.featureType.IMPORTANT, NHContent.icon2),
-                new FeatureLog(0, FeatureLog.featureType.FEATURE, NHContent.objective),
-                new FeatureLog(1, FeatureLog.featureType.FEATURE, NHContent.fleet),
-                new FeatureLog(2, FeatureLog.featureType.FEATURE, Core.atlas.find("new-horizon-incursion")),
-                new FeatureLog(3, FeatureLog.featureType.FEATURE, NHContent.capture),
-                new FeatureLog(4, FeatureLog.featureType.FEATURE, NHContent.raid),
-
-                new FeatureLog(0, FeatureLog.featureType.CONTENT, NHContent.objective),
-                new FeatureLog(1, FeatureLog.featureType.CONTENT, NHContent.objective),
-                new FeatureLog(2, FeatureLog.featureType.CONTENT, NHContent.objective),
-                new FeatureLog(3, FeatureLog.featureType.CONTENT, NHContent.objective),
-
-                new FeatureLog(1, FeatureLog.featureType.IMPORTANT, NHContent.pointerRegion)
+                new FeatureLog(0, FeatureLog.featureType.IMPORTANT, NHContent.icon2)
         );
-
-        ModuleBlock.modules.each(module -> updates.add(new FeatureLog(module)));
-
-        updates.add(new FeatureLog(CraftingBlock.stampingFacility));
-        updates.add(new FeatureLog(CraftingBlock.processorPrinter));
-        updates.add(new FeatureLog(CraftingBlock.crucibleFoundry));
-        updates.add(new FeatureLog(CraftingBlock.crystallizer));
-        updates.add(new FeatureLog(CraftingBlock.surgeRefactor));
-        updates.add(new FeatureLog(CraftingBlock.fabricSynthesizer));
-        updates.add(new FeatureLog(CraftingBlock.processorEncoder));
-        updates.add(new FeatureLog(CraftingBlock.irdryonMixer));
-        updates.add(new FeatureLog(CraftingBlock.multipleSteelFactory));
-        updates.add(new FeatureLog(CraftingBlock.irayrondFactory));
-        updates.add(new FeatureLog(CraftingBlock.setonFactory));
-        updates.add(new FeatureLog(CraftingBlock.upgradeSortFactory));
-        updates.add(new FeatureLog(CraftingBlock.ancimembraneConcentrator));
-        updates.add(new FeatureLog(CraftingBlock.factory0));
-        updates.add(new FeatureLog(CraftingBlock.factory1));
-        updates.add(new FeatureLog(CraftingBlock.factory2));
-        updates.add(new FeatureLog(CraftingBlock.factory3));
-        updates.add(new FeatureLog(CraftingBlock.factory4));
-        updates.add(new FeatureLog(CraftingBlock.factory5));
-        updates.add(new FeatureLog(CraftingBlock.factory6));
-
-        updates.add(new FeatureLog(LiquidBlock.turboPumpSmall));
-        updates.add(new FeatureLog(LiquidBlock.turboPump));
-        updates.add(new FeatureLog(LiquidBlock.standardLiquidStorage));
-        updates.add(new FeatureLog(LiquidBlock.heavyLiquidStorage));
-
-        updates.add(new FeatureLog(LogicBlock.iconDisplay));
-        updates.add(new FeatureLog(LogicBlock.characterDisplay));
-
-        updates.add(new FeatureLog(PayloadBlock.payloadRail));
-        updates.add(new FeatureLog(PayloadBlock.payloadRouter));
-
-        updates.add(new FeatureLog(PowerBlock.gravityTrapMidantha));
-        updates.add(new FeatureLog(PowerBlock.gravityTrapSmall));
-        updates.add(new FeatureLog(PowerBlock.gravityTrap));
-        updates.add(new FeatureLog(PowerBlock.armorBattery));
-        updates.add(new FeatureLog(PowerBlock.armorBatteryLarge));
-        updates.add(new FeatureLog(PowerBlock.armorBatteryHuge));
-        updates.add(new FeatureLog(PowerBlock.zetaGenerator));
-        updates.add(new FeatureLog(PowerBlock.anodeFusionReactor));
-        updates.add(new FeatureLog(PowerBlock.cathodeFusionReactor));
-        updates.add(new FeatureLog(PowerBlock.thermoReactor));
-
-        updates.add(new FeatureLog(SpecialBlock.juniorModuleBeacon));
-        updates.add(new FeatureLog(SpecialBlock.seniorModuleBeacon));
-
-        updates.add(new FeatureLog(DefenseBlock.standardForceProjector));
-        updates.add(new FeatureLog(DefenseBlock.largeShieldGenerator));
-        updates.add(new FeatureLog(DefenseBlock.standardRegenProjector));
-        updates.add(new FeatureLog(DefenseBlock.heavyRegenProjector));
-
         return updates.toArray(FeatureLog.class);
     }
 
