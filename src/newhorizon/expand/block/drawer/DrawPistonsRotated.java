f0df7eefbacabdb53326f425d5f88dc9b26ec279
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-10-29 14:40:42 +0800

    Add files via upload

---
+++ src/newhorizon/expand/block/drawer/DrawPistonsRotated.java
@@ -0,0 +1,29 @@
+package newhorizon.expand.block.drawer;
+
+import arc.math.geom.Vec2;
+import arc.graphics.g2d.Draw;
+import mindustry.gen.Building;
+import mindustry.world.draw.DrawPistons;
+
+public class DrawPistonsRotated extends DrawPistons {
+    public float x = 0f, y = 0f;
+
+    public DrawPistonsRotated() {
+        super();
+    }
+
+    @Override
+    public void draw(Building build) {
+        Vec2 pos = new Vec2(x, y).rotate(build.rotdeg()).add(build.x, build.y);
+
+        for (int i = 0; i < sides; i++) {
+            float angle = ((float) i / sides) * 360f + sideOffset + build.rotdeg();
+            float offset = (float) Math.sin((build.totalProgress() * sinScl) + i) * sinMag;
+
+            float dx = pos.x + (float) Math.cos(Math.toRadians(angle)) * offset;
+            float dy = pos.y + (float) Math.sin(Math.toRadians(angle)) * offset;
+
+            Draw.rect(region, dx, dy, angle);
+        }
+    }
+}
