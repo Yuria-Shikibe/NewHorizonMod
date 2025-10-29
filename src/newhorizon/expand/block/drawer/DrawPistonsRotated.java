498dcfc732193ebba46c92500d8eaa038a557150
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-10-29 15:37:59 +0800

    content

--- src/newhorizon/expand/block/drawer/DrawPistonsRotated.java
+++ src/newhorizon/expand/block/drawer/DrawPistonsRotated.java
@@ -1,10 +1,11 @@
 package newhorizon.expand.block.drawer;
 
-import arc.graphics.g2d.Draw;
-import arc.math.geom.Vec2;
-import mindustry.gen.Building;
-import arc.graphics.g2d.TextureRegion;
-import mindustry.world.draw.DrawBlock;
+import arc.graphics.g2d.*;
+import arc.math.geom.*;
+import arc.util.*;
+import mindustry.gen.*;
+import mindustry.world.*;
+import mindustry.world.draw.*;
 
 public class DrawPistonsRotated extends DrawBlock {
     public float x = 0f, y = 0f;
@@ -14,24 +15,24 @@ public class DrawPistonsRotated extends DrawBlock {
     public float sinScl = 5f;
     public float sideOffset = 0f;
 
-    public DrawPistonsRotated() {
-    }
-
-    public DrawPistonsRotated(TextureRegion region) {
-        this.region = region;
+    @Override
+    public void load(Block block) {
+        region = Core.atlas.find(block.name + "-piston", block.region);
     }
 
     @Override
     public void draw(Building build) {
-        if (region == null) return;
+        if(region == null) return;
 
-        Vec2 pos = new Vec2(x, y).rotate(build.rotdeg()).add(build.x, build.y);
+        Vec2 pos = Tmp.v1.trns(build.rotdeg(), x, y).add(build.x, build.y);
 
-        for (int i = 0; i < sides; i++) {
-            float angle = ((float) i / sides) * 360f + sideOffset + build.rotdeg();
-            float offset = (float) Math.sin((build.totalProgress() * sinScl) + i) * sinMag;
-            float dx = pos.x + (float) Math.cos(Math.toRadians(angle)) * offset;
-            float dy = pos.y + (float) Math.sin(Math.toRadians(angle)) * offset;
+        for(int i = 0; i < sides; i++){
+            float angle = i * 360f / sides + sideOffset + build.rotdeg();
+            float offset = Mathf.sin(build.totalProgress() * sinScl + i) * sinMag;
+
+            float dx = pos.x + Mathf.cosDeg(angle) * offset;
+            float dy = pos.y + Mathf.sinDeg(angle) * offset;
+
             Draw.rect(region, dx, dy, angle);
         }
     }
