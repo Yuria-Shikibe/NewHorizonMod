c44fe105d503d3ab00080313eac69aaca3157c83
zzrte33 <142125522+zzrte33@users.noreply.github.com>
2025-10-29 16:46:30 +0800

    content

--- src/newhorizon/expand/block/drawer/DrawPistonsRotated.java
+++ src/newhorizon/expand/block/drawer/DrawPistonsRotated.java
@@ -13,9 +13,9 @@ import mindustry.world.draw.DrawBlock;
 public class DrawPistonsRotated extends DrawBlock {
     public float x = 0f, y = 0f;
     public TextureRegion region;
-    public int sides = 8;
-    public float sinMag = 2.75f;
-    public float sinScl = 5f;
+    public int sides = 4;
+    public float sinMag = 4f;
+    public float sinScl = 6f;
     public float sideOffset = 0f;
 
     @Override
@@ -28,6 +28,8 @@ public class DrawPistonsRotated extends DrawBlock {
         if(region == null) return;
 
         Vec2 pos = Tmp.v1.trns(build.rotdeg(), x, y).add(build.x, build.y);
+
+        float progress = Time.time * sinScl;
 
         for(int i = 0; i < sides; i++){
             float angle = i * 360f / sides + sideOffset + build.rotdeg();
