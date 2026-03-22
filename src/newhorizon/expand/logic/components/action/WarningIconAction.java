944a924ba4f4ddfa701dfd8ad84f09d68298839e
LaoHuaJi <laohuaji233@gmail.com>
2026-03-23 00:39:20 +0800

    wip

--- src/newhorizon/expand/logic/components/action/WarningIconAction.java
+++ src/newhorizon/expand/logic/components/action/WarningIconAction.java
@@ -49,9 +49,17 @@ public class WarningIconAction extends Action {
         NHUIFunc.showLabel(duration / Time.toSeconds, t -> {
             t.background(Styles.black5);
             t.table(t2 -> {
-                t2.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padRight(-9).color(team.color);
+                t2.table(left -> {
+                    //left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padBottom(54f).padRight(-12).color(team.color).row();
+                    left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padRight(-9).color(team.color).row();
+                    //left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padTop(54f).padRight(-32).color(team.color).row();
+                }).pad(0).growX();
                 t2.image(warningIcon()).fill().color(team.color);
-                t2.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padLeft(-9).color(team.color);
+                t2.table(right -> {
+                    //right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padBottom(54f).padLeft(-32).color(team.color).row();
+                    right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padLeft(-9).color(team.color).row();
+                    //right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padTop(54f).padLeft(-12).color(team.color).row();
+                }).pad(0).growX();
             }).growX().pad(OFFSET / 2).fillY().row();
 
             t.table(l -> l.add(new FLabel("<< " + text + " >>")).color(team.color).padBottom(4).row()).growX().fillY();
