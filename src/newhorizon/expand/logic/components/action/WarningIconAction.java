8f3192fb14a3e7dab641a68ae3778bfbb8b37238
LaoHuaJi <laohuaji233@gmail.com>
2026-03-24 00:15:45 +0800

    wip

--- src/newhorizon/expand/logic/components/action/WarningIconAction.java
+++ src/newhorizon/expand/logic/components/action/WarningIconAction.java
@@ -50,14 +50,20 @@ public class WarningIconAction extends Action {
             t.background(Styles.black5);
             t.table(t2 -> {
                 t2.table(left -> {
+                    left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).pad(-42, 0, 0, -40).color(team.color).row();
+                    left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).pad(0, 0, -42, -40).color(team.color).row();
+
                     //left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padBottom(54f).padRight(-12).color(team.color).row();
-                    left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padRight(-9).color(team.color).row();
+                    //left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padRight(-9).color(team.color).row();
                     //left.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padTop(54f).padRight(-32).color(team.color).row();
                 }).pad(0).growX();
                 t2.image(warningIcon()).fill().color(team.color);
                 t2.table(right -> {
+                    right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).pad(-42, -40, 0, 0).color(team.color).row();
+                    right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).pad(0, -40, -42, 0).color(team.color).row();
+
                     //right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padBottom(54f).padLeft(-32).color(team.color).row();
-                    right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padLeft(-9).color(team.color).row();
+                    //right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padLeft(-9).color(team.color).row();
                     //right.image().growX().height(OFFSET / 2).pad(OFFSET / 3).padTop(54f).padLeft(-12).color(team.color).row();
                 }).pad(0).growX();
             }).growX().pad(OFFSET / 2).fillY().row();
